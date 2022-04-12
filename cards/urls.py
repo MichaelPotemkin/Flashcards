38fd235a3fede6d5c422e493bce33723c87e809d
Michael Potyomkin <54860902+BioAutomaton@users.noreply.github.com>
2022-04-12 15:39:23 +0300

    Add like button

--- cards/urls.py
+++ cards/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('create/<pack_id>', views.create_pack, name='create'),
     # path('edit/<int:pack_id>', views.edit_pack, name='edit'),
     # path('delete/<int:pack_id>', views.delete_pack, name='delete'),
+    path('like/<int:pk>', views.like_pack, name='like_pack')
 ]
