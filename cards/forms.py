7200c2be991029af582b443e458ec814ae65e763
Michael Potyomkin <54860902+BioAutomaton@users.noreply.github.com>
2022-04-24 17:05:56 +0300

    Minor changes

--- cards/forms.py
+++ cards/forms.py
@@ -4,6 +4,16 @@ from django import forms
 
 
 class CreatePackForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].reqired = True
+        self.fields['title'].label = "Название"
+
+        for field in self.Meta.fields:
+            self.fields[field].required = False
+            self.fields[field].label = ''
+
+
     class Meta:
         model = Pack
         fields = ['title']
